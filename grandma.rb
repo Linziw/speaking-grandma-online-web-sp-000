f003e31d22d926eaeb785e442b1e9efcf30c766f
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-19 11:14:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,8 @@ def speak_to_grandma(string)
   if string == string.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return 
-  
-end
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
